fb7f6b2df95d42a228d2290f5052ab14d0eccd71
Balghari <najmbalghari13@gmail.com>
2024-02-06 19:30:50 +0500

    drag and drop project using for of loop

---
+++ drag and drop/drag.js
@@ -0,0 +1,25 @@
+let lists = document.querySelectorAll("#item");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for (const list of lists) {
+  list.addEventListener("dragstart", function (e) {
+    let selected = e.target;
+
+    rightBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    rightBox.addEventListener("drop", function () {
+      rightBox.appendChild(selected);
+      selected = null;
+    });
+
+    leftBox.addEventListener('dragover', function(e){
+      e.preventDefault()
+    })
+    leftBox.addEventListener('drop', function(){
+      leftBox.appendChild(selected)
+      selected=null
+    })
+  });
+}
